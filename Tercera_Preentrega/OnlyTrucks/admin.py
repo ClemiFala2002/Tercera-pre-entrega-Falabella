d07739aa75b10a8d2c760276326050f1a1a2629a
ClemiFala2002 <clemifala13@gmail.com>
2023-03-14 20:59:41 -0300

    v2.0

--- Tercera_Preentrega/OnlyTrucks/admin.py
+++ Tercera_Preentrega/OnlyTrucks/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from OnlyTrucks.models import Post
 
-# Register your models here.
+admin.site.register(Post)
---
+++ Tercera_Preentrega/seed_data.py
@@ -0,0 +1,32 @@
+from OnlyTrucks.models import Post
+
+Post(carousel_caption_title="un carousel title",
+     carousel_caption_description="un carousel description",
+     heading="mi camion",
+     description="una descripcion",
+     un_campo=""
+     ).save()
+
+
+Post(carousel_caption_title="un carousel title1",
+     carousel_caption_description="un carousel description1",
+     heading="mi camion1",
+     description="una descripcion1",
+     un_campo=""
+     ).save()
+
+
+Post(carousel_caption_title="un carousel title2",
+     carousel_caption_description="un carousel description2",
+     heading="mi camion2",
+     description="una descripcion2",
+     un_campo=""
+     ).save()
+
+
+Post(carousel_caption_title="un carousel title3",
+     carousel_caption_description="un carousel description3",
+     heading="mi camion3",
+     description="una descripcion3",
+     un_campo=""
+     ).save()
